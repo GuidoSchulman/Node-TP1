a6f273737ae4b705e37a1130905f70c5911950f5
GUIDO SCHULMAN <46961979@ort>
2023-04-04 09:06:25 -0300

    4/4

---
+++ EJ4.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+const entradaInvalida = "./entradaInvalida.txt";
+const entrada = "./entrada.txt";
+const salida = "./salida.txt";
+
+/*
+function copiarArchivo(){
+    console.log("copiar");
+    copiar(entrada, salida);
+}
+*/
